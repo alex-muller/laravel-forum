d21414ed14a8bb7c492de58d710f8f25ac898db2
Alex Muller <mullerbox@gmail.com>
2018-03-14 19:52:42 +0200

    15

---
+++ app/Filters/Filters.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex
+ * Date: 3/13/18
+ * Time: 8:07 PM
+ */
+
+namespace App\Filters;
+
+
+use Illuminate\Http\Request;
+
+abstract class Filters
+{
+
+    protected $request, $builder;
+
+    protected $filters = [];
+
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public function apply($builder)
+    {
+
+        $this->builder = $builder;
+
+        foreach ($this->getFilters() as $filter => $value){
+            if (method_exists($this, $filter)){
+                $this->$filter($value);
+            }
+        }
+
+        return $this->builder;
+    }
+
+    public function getFilters()
+    {
+        return $this->request->only($this->filters);
+    }
+
+
+}
---
+++ tests/Feature/ParticipateInForumTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class ParticipateInForumTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->withExceptionHandling();
+        $this->post('/threads/1/1/replies', [])
+             ->assertRedirect('/login');
+    }
+
+    /** @test */
+    function an_authenticated_user_may_participate_in_forum_threads()
+    {
+        $user = factory('App\User')->create();
+        $this->be($user);
+        $thread = factory('App\Thread')->create();
+
+        $reply = factory('App\Reply')->make();
+        $this->post($thread->path().'/replies', $reply->toArray());
+
+        $this->get($thread->path())
+             ->assertSee($reply->body);
+    }
+
+    /** @test */
+    function a_reply_requires_a_body()
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = factory('App\Thread')->create();
+        $reply = make('App\Reply', ['body' => null]);
+        $this->post($thread->path().'/replies', $reply->toArray())
+             ->assertSessionHasErrors('body');
+    }
+}
