cbf8bebbdbfe081955cde024bf778ff415dcbdb4
Alex Muller <mullerbox@gmail.com>
2018-03-21 16:41:45 +0200

    27

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -11,19 +11,8 @@ class ProfilesController extends Controller
     {
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $this->getActivity($user)
+            'activities' => \App\Activity::feed($user)
         ]);
     }
 
-    /**
-     * @param User $user
-     *
-     * @return static
-     */
-    protected function getActivity(User $user)
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
-    }
 }
