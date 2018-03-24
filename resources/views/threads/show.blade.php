379fb1e193855e7e19a7a30b7101c4caade3a8b9
Alex Muller <mullerbox@gmail.com>
2018-03-24 20:42:07 +0200

    36

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -1,59 +1,61 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <div class="level">
-             <span class="flex"><a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
+  <thread-view initial-replies-count="{{ $thread->replies_count }}" inline-template>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <div class="level">
+              <span class="flex"><a href="/profiles/{{ $thread->creator->name }}">{{ $thread->creator->name }}</a> posted:
                {{ $thread->title }}</span>
-              @can('update', $thread)
-              <form action="{{ $thread->path() }}" method="POST">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit" class="btn btn-link">Delete Thread</button>
-              </form>
-              @endcan
-            </div>            
-          </div>
-          <div class="panel-body">
-           {{ $thread->body }}
-          </div>
-        </div>
-
-        @foreach($replies as $reply)
-          @include('threads.reply')
-        @endforeach
-
-        {{ $replies->links() }}
-
-        @auth()
-          <form method="POST" action="{{ $thread->path() . '/replies' }}">
-            {{ csrf_field() }}
-            <div class="form-group">
-              <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"></textarea>
+                @can('update', $thread)
+                  <form action="{{ $thread->path() }}" method="POST">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button type="submit" class="btn btn-link">Delete Thread</button>
+                  </form>
+                @endcan
+              </div>
             </div>
+            <div class="panel-body">
+              {{ $thread->body }}
+            </div>
+          </div>
 
-            <button type="submit" class="btn btn-default">Post</button>
-          </form>
-        @endauth
-        @guest()
-          <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion</p>
-        @endguest
-      </div>
+          <replies :data="{{ $thread->replies }}" @removed="repliesCount--"></replies>
 
-      <div class="col-md-4">
-        <div class="panel panel-default">
-          <div class="panel-body">
-            <p>This thread was published {{ $thread->created_at->diffForHumans() }} by 
-              <a href="#">{{ $thread->creator->name }}</a>, and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}.</p>
+          {{--{{ $replies->links() }}--}}
+
+          @auth()
+            <form method="POST" action="{{ $thread->path() . '/replies' }}">
+              {{ csrf_field() }}
+              <div class="form-group">
+                <textarea name="body" id="body" class="form-control" placeholder="Have something to say?"></textarea>
+              </div>
+
+              <button type="submit" class="btn btn-default">Post</button>
+            </form>
+          @endauth
+          @guest()
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion
+            </p>
+          @endguest
+        </div>
+
+        <div class="col-md-4">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <p>This thread was published {{ $thread->created_at->diffForHumans() }} by
+                <a href="#">{{ $thread->creator->name }}</a>, and currently
+                has <span v-text="repliesCount"></span> {{ str_plural('comment', $thread->replies_count) }}.</p>
+            </div>
           </div>
         </div>
-      </div>
 
+      </div>
     </div>
-  </div>
+  </thread-view>
 
 @endsection
