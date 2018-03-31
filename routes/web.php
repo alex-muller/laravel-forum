793b4f2298ef662a133512b203ec840161855b09
Alex Muller <mullerbox@gmail.com>
2018-04-01 00:06:54 +0300

    42

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::post('/threads', 'ThreadsController@store');
 Route::get('/threads/{channel}/{thread}/replies', 'RepliesController@index');
 Route::post('/threads/{channel}/{thread}/replies', 'RepliesController@store');
 Route::post('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@store')->middleware('auth');
+Route::delete('/threads/{channel}/{thread}/subscriptions', 'ThreadSubscriptionsController@destroy')->middleware('auth');
 
 Route::patch('/replies/{reply}', 'RepliesController@update');
 Route::delete('/replies/{reply}', 'RepliesController@destroy');
