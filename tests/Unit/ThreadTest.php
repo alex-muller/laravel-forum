da8ea664b350c568d6e8e33ecfaa26d8b6f1f0ec
Alex Muller <mullerbox@gmail.com>
2018-04-04 10:54:18 +0300

    48

--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use App\Notifications\ThreadWasUpdated;
 use App\User;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Support\Facades\Notification;
@@ -114,6 +115,26 @@ class ThreadTest extends TestCase
         $thread->subscribe();
 
         $this->assertTrue($thread->isSubscribedTo);
+    }
+
+    /** @test */
+    function a_thread_can_check_if_the_authenticated_user_has_read_all_replies()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        tap(auth()->user(), function($user) use ($thread){
+
+            $this->assertTrue($thread->hasUpdatesFor($user));
+
+            $user->read($thread);
+
+            $this->assertFalse($thread->hasUpdatesFor($user));
+
+        });
+
+
     }
 
 }
