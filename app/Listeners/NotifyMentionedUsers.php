420ee9d5d139a7b6b8ad4287f2f7c1cae4bfb8e3
Alex Muller <mullerbox@gmail.com>
2018-04-06 18:40:39 +0300

    58

--- app/Listeners/NotifyMentionedUsers.php
+++ app/Listeners/NotifyMentionedUsers.php
@@ -18,12 +18,7 @@ class NotifyMentionedUsers
      */
     public function handle(ThreadReceivedNewReply $event)
     {
-
-        collect($event->reply->mentionedUsers())
-            ->map(function ($name) {
-                return User::whereName($name)->first();
-            })
-            ->filter()
+        User::whereIn('name', $event->reply->mentionedUsers())
             ->each(function ($user) use ($event){
                 $user->notify(new YouWereMentioned($event->reply));
             });
